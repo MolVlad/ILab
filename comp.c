de40ac001b3e54f2151c567dc63b10a8929146b3
MolVlad <molodcov.vladik@gmail.com>
2018-04-12 16:30:36 +0300

    lol

---
+++ comp.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <string.h>
+
+enum {BIN_OPERATOR, OPERATOR, NUMBER, REGISTER, PUNCTUATION, FUNCTION, CONST};
+
+enum {PLUS, MINUS, DIVIDE, MULTIPLY};	// BIN_OPERATOR
+enum {SIN, COS, TG, ARCSIN, ARCTG, EXP, LOG, LN, DEGREE}	//OPERATOR
+enum {RAX, RBX, RCX, RDX};	// REGISTER
+enum {IN, OUT, PUSH, POP, ADD, SUB, MUL, DIV};	// FUNCTION
+
+
+
+struct 	
+
+
+
+
+
+
+
+
+

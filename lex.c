645e3aac2b42b818a51db244c45c28db457ceacf
MolVlad <molodcov.vladik@gmail.com>
2018-04-15 08:07:46 +0300

    lexical an ended

--- lex.c
+++ lex.c
@@ -27,7 +27,7 @@ char * FileToStr()
 	printf("---|Исходный текст программы|---\n");
 	printf("%s", str);
 	printf("---|Исходный текст программы|---\n\n");
-//	fclose(file);
+	fclose(file);
 
 	return str;
 }
@@ -55,12 +55,8 @@ Token * LexicalAnalysis(char * str)
 
 	while(position < size)
 	{
-		if(str[position] == '\n' || str[position] == ' ')
-			position++;
-		else if(str[position] == '(' || str[position] == ')')
-			position++;
-//---|BIN_OPERATORS|---
-		else if(str[position] == '+')
+		//---|BIN_OPERATORS|---
+		if(str[position] == '+')
 		{
 			tokens = CreateToken(tokens, BIN_OPERATOR, PLUS);
 			position++;
@@ -85,99 +81,128 @@ Token * LexicalAnalysis(char * str)
 			tokens = CreateToken(tokens, BIN_OPERATOR, DEGREE);
 			position++;
 		}
-//---|OPERATORS|---
-		else if(str[position] == 's' && str[position + 1] == 'i' && str[position + 2] == 'n')
+		//---|OPERATORS|---
+		else if(str[position] == 's' && str[position + 1] == 'i' && str[position + 2] == 'n' && str[position + 3] == '(')
 		{
 			position += 3;
-			assert(str[position] == '(');
 			tokens = CreateToken(tokens, OPERATOR, SIN);
 		}
-		else if(str[position] == 'c' && str[position + 1] == 'o' && str[position + 2] == 's')
+		else if(str[position] == 'c' && str[position + 1] == 'o' && str[position + 2] == 's' && str[position + 3] == '(')
 		{
 			position += 3;
-			assert(str[position] == '(');
 			tokens = CreateToken(tokens, OPERATOR, COS);
 		}
-		else if(str[position] == 't' && str[position + 1] == 'g')
+		else if(str[position] == 't' && str[position + 1] == 'g' && str[position + 2] == '(')
 		{
 			position += 2;
-			assert(str[position] == '(');
 			tokens = CreateToken(tokens, OPERATOR, TG);
 		}
-		else if(str[position] == 'l' && str[position + 1] == 'n')
+		else if(str[position] == 'l' && str[position + 1] == 'n' && str[position + 2] == '(')
 		{
 			position += 2;
-			assert(str[position] == '(');
 			tokens = CreateToken(tokens, OPERATOR, LN);
 		}
-//---|REGISTERS|---
+		//---|REGISTERS|---
 		else if(str[position] == 'r'&& str[position + 1] == 'a' && str[position + 2] == 'x'
-				&& (str[position + 3] == ' ' || str[position + 3] == '\n' || str[position] == ')'))
+				&& (str[position + 3] == ' ' || str[position + 3] == '\n' || str[position + 3] == ')'))
 		{
 			position += 3;
 			tokens = CreateToken(tokens, REGISTER, RAX);
 		}
 		else if(str[position] == 'r'&& str[position + 1] == 'b' && str[position + 2] == 'x'
-				&& (str[position + 3] == ' ' || str[position + 3] == '\n' || str[position] == ')'))
+				&& (str[position + 3] == ' ' || str[position + 3] == '\n' || str[position + 3] == ')'))
 		{
 			position += 3;
 			tokens = CreateToken(tokens, REGISTER, RBX);
 		}
 		else if(str[position] == 'r'&& str[position + 1] == 'c' && str[position + 2] == 'x'
-				&& (str[position + 3] == ' ' || str[position + 3] == '\n' || str[position] == ')'))
+				&& (str[position + 3] == ' ' || str[position + 3] == '\n' || str[position + 3] == ')'))
 		{
 			position += 3;
 			tokens = CreateToken(tokens, REGISTER, RCX);
 		}
 		else if(str[position] == 'r'&& str[position + 1] == 'd' && str[position + 2] == 'x'
-				&& (str[position + 3] == ' ' || str[position + 3] == '\n' || str[position] == ')'))
+				&& (str[position + 3] == ' ' || str[position + 3] == '\n' || str[position + 3] == ')'))
 		{
 			position += 3;
 			tokens = CreateToken(tokens, REGISTER, RDX);
 		}
+		//---|PUNCTUATION|---
+		else if(str[position] == '(')
+		{
+			position++;
+			tokens = CreateToken(tokens, PUNCTUATION, BRACKET_OPENING);
+		}
+		else if(str[position] == ')')
+		{
+			position++;
+			tokens = CreateToken(tokens, PUNCTUATION, BRACKET_CLOSE);
+		}
+		else if(str[position] == '\n')
+		{
+			position++;
+			tokens = CreateToken(tokens, PUNCTUATION, NEW_LINE);
+		}
+		else if(str[position] == '=')
+		{
+			position++;
+			tokens = CreateToken(tokens, PUNCTUATION, EQUALITY);
+		}
+		else if(str[position] == ' ')
+		{
+			position++;
+		}
+		//---|FUNCTIONS|---
+		else if(str[position] == 'i' && str[position + 1] == 'n'
+				&& (str[position + 2] == '\n' || str[position + 2] == ' '))
+		{
+			position += 2;
+			tokens = CreateToken(tokens, FUNCTION, IN);
+		}
+		else if(str[position] == 'o' && str[position + 1] == 'u' && str[position + 2] == 't'
+				&& (str[position + 3] == '\n' || str[position + 3] == ' '))
+		{
+			position += 3;
+			tokens = CreateToken(tokens, FUNCTION, OUT);
+		}
+		else if(str[position] == 'p' && str[position + 1] == 'u' && str[position + 2] == 's' && str[position + 3] == 'h'
+				&& (str[position + 4] == '\n' || str[position + 4] == ' '))
+		{
+			position += 4;
+			tokens = CreateToken(tokens, FUNCTION, PUSH);
+		}
+		else if(str[position] == 'p' && str[position + 1] == 'o' && str[position + 2] == 'p'
+				&& (str[position + 3] == '\n' || str[position + 3] == ' '))
+		{
+			position += 4;
+			tokens = CreateToken(tokens, FUNCTION, POP);
+		}
+		else if(str[position] == 'a' && str[position + 1] == 'd' && str[position + 2] == 'd'
+				&& (str[position + 3] == '\n' || str[position + 3] == ' '))
+		{
+			position += 3;
+			tokens = CreateToken(tokens, FUNCTION, ADD);
+		}
+		else if(str[position] == 's' && str[position + 1] == 'u' && str[position + 2] == 'b'
+				&& (str[position + 3] == '\n' || str[position + 3] == ' '))
+		{
+			position += 3;
+			tokens = CreateToken(tokens, FUNCTION, SUB);
+		}
+		else if(str[position] == 'm' && str[position + 1] == 'u' && str[position + 2] == 'l'
+				&& (str[position + 3] == '\n' || str[position + 3] == ' '))
+		{
+			position += 3;
+			tokens = CreateToken(tokens, FUNCTION, MUL);
+		}
+		else if(str[position] == 'd' && str[position + 1] == 'i' && str[position + 2] == 'v'
+				&& (str[position + 3] == '\n' || str[position + 3] == ' '))
+		{
+			position += 3;
+			tokens = CreateToken(tokens, FUNCTION, DIV);
+		}
 		else
 			assert(!"wtf");
-/*//---|FUNCTIONS|---
-		if()
-		{
-			
-			CreateToken(tokens, FUNCTION, );
-		}
-		if()
-		{
-			
-			CreateToken(tokens, FUNCTION, );
-		}
-		if()
-		{
-			
-			CreateToken(tokens, FUNCTION, );
-		}
-		if()
-		{
-			
-			CreateToken(tokens, FUNCTION, );
-		}
-		if()
-		{
-			
-			CreateToken(tokens, FUNCTION, );
-		}
-		if()
-		{
-			
-			CreateToken(tokens, FUNCTION, );
-		}
-		if()
-		{
-			
-			CreateToken(tokens, FUNCTION, );
-		}
-		if()
-		{
-			
-			CreateToken(tokens, FUNCTION, );
-		}*/
 	}
 
 	printf("---|Лексический анализ завершен|---\n");
@@ -250,9 +275,9 @@ char * GetSemantics(Token * token)
 				case COS:
 					return "cos";
 				case TG:
-					return "TG";
+					return "tg";
 				case LN:
-					return "LN";
+					return "ln";
 				default:
 					assert(!"wtf");
 			}
@@ -270,6 +295,38 @@ char * GetSemantics(Token * token)
 					return "rdx";
 			}
 			break;
+		case PUNCTUATION:
+			switch(token->Value)
+			{
+				case BRACKET_OPENING:
+					return "(";
+				case BRACKET_CLOSE:
+					return ")";
+				case NEW_LINE:
+					return "\\\\n";
+				case EQUALITY:
+					return "=";
+			}
+		case FUNCTION:
+			switch(token->Value)
+			{
+				case IN:
+					return "in";
+				case OUT:
+					return "out";
+				case PUSH:
+					return "push";
+				case POP:
+					return "pop";
+				case ADD:
+					return "add";
+				case SUB:
+					return "sub";
+				case MUL:
+					return "mul";
+				case DIV:
+					return "div";
+			}
 		default:
 			assert(!"wtf");
 	}
@@ -281,7 +338,7 @@ Token * CreateToken(Token * tokens, int type, int value)
 	{
 		tokens = calloc(1, sizeof(Token));
 		tokens->Next = NULL;
-		tokens->Type - type;
+		tokens->Type = type;
 		tokens->Value = value;
 
 		return tokens;
@@ -291,8 +348,3 @@ Token * CreateToken(Token * tokens, int type, int value)
 
 		return tokens;
 }
-
-
-
-
-

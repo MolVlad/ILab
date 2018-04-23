981792c9ea323f3d5125debf52601a4f1933f94c
MolVlad <molodcov.vladik@gmail.com>
2018-04-23 16:18:34 +0300

    lol

--- check.h
+++
@@ -1,5 +0,0 @@
-void Check(Token * tokens);
-void ErrorMessage(Token * tokens);
-int Check_GetG(Token * tokens);
-
-int pos_check;
--- run.h
+++
@@ -1,7 +0,0 @@
-void Run(Token * tokens);
-int Run_GetG(Token * tokens);
-int Run_GetE(Token * tokens);
-int Run_GetT(Token * tokens);
-int Run_GetP(Token * tokens);
-int Run_GetN(Token * tokens);
-
